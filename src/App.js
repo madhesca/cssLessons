84fac2e6a49972cc8eb487426b09cd4190beef48
Marton <mpochavillo@gmail.com>
2020-08-05 19:32:39 +0800

    continue

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from "react";
-import "./App.css";
-import NavButtons from "./components/NavButtons/NavButtons";
-import Forms from "./components/Forms/Forms";
+import React from "react"
+import "./App.css"
+import NavButtons from "./components/NavButtons/NavButtons"
+import Forms from "./components/Forms/Forms"
+import Opacity from "./components/Opacity/Opacity"
 
 function App() {
   return (
     <div>
-      <NavButtons />
-      <Forms />
+      <Opacity />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Opacity/Opacity.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./Opacity.css"
+
+function Opacity() {
+  return (
+    <div>
+      <div className="box1"></div>
+      <div className="box2"></div>
+    </div>
+  )
+}
+
+export default Opacity
