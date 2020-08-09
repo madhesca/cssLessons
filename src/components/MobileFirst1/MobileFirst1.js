37de5895a04c9983186f97187289e067405a195c
marton <mpochavillo@gmail.com>
2020-08-09 09:14:21 +0800

    done with the project

---
+++ src/components/MobileFirst1/MobileFirst1.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./MobileFirst1.css";
+
+function MobileFirst1() {
+  return (
+    <div>
+      <img
+        className="banner"
+        src="https://marketplace.canva.com/EAD7TRhFbfg/1/0/800w/canva-simple-work-linkedin-banner-o8RYd-hyoQI.jpg"
+        alt="banner"
+      />
+
+      <div className="main">
+        <div className="texts">
+          <h1> Kobe Bryant </h1>
+          <p>
+            <strong>Kobe Bean Bryant</strong> (/ˈkoʊbiː/ KOH-bee; August 23,
+            1978 – January 26, 2020) was an American professional basketball
+            player for the Los Angeles Lakers in the National Basketball
+            Association (NBA). As a shooting guard, he entered the league
+            directly from high school, and played his entire 20-season
+            professional career with the Lakers. Bryant won many accolades: five
+            NBA championships, 18-time All-Star, 15-time member of the All-NBA
+            Team, 12-time member of the All-Defensive Team, 2008 NBA Most
+            Valuable Player (MVP), two-time NBA Finals MVP winner. Widely
+            regarded as one of the greatest players of all time,[3][4][5][6] he
+            led the NBA in scoring during two seasons, ranks fourth on the
+            league's all-time regular season scoring and all-time postseason
+            scoring lists.
+          </p>
+        </div>
+
+        <img
+          className="kobe"
+          src="https://image.cnbcfm.com/api/v1/image/101524695-457220551.jpg?v=1395781183&w=1400&h=950"
+          alt="kobe"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default MobileFirst1;
